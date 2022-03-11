5f5d3968bd36ccc087043dd2c6766db1653b96c5
Kenny <mccormick84@gmail.com>
2022-03-12 00:26:22 +0900

    Practice flutter row and column

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:row_and_column/screen/home_screen.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomeScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
